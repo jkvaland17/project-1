61d2c00757c11a296a081a1522597087f132f0c6
jkvaland17 <108795457+jkvaland17@users.noreply.github.com>
2022-08-29 19:19:35 +0530

    hghghgh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./Header.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home.js";
+import Service from "./Component/Service.js";
+import About from "./Component/About.js";
+import Contact from "./Component/Contact.js";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/service" element={<Service />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from "react-router-dom";
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
